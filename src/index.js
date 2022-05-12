c210cea37c12858bfaa78d110cb23dd1b10a0f5a
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-05-12 21:32:21 +0200

    Added styling file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Form from './components/Form';
+import "/home/kamo/cv-creator/src/styles/app.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>CV Creator</h1>
+      <h1 id="header1">CV Creator</h1>
     </div>
     <Form />
   </React.StrictMode>
