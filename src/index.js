84c6de61c59679914186d6072412acaa7ff2d0af
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-05-17 21:10:19 +0200

    Changed imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Form from './components/Form';
+import Form, { Formf } from './components/Form';
 import "/home/kamo/cv-creator/src/styles/app.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1 id="header1">CV Creator</h1>
-    </div>
-    <Form />
+    <Formf />
   </React.StrictMode>
 );
